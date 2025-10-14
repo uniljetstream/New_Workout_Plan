f2cbb750b68c7183d0623cdf04b2f5f844bf88ab
uniljetstream <email@email.com>
2025-10-14 09:42:31 +0900

    qt 앱 실패

--- Qt_app/ui_settings.h
+++ Qt_app/ui_settings.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'settings.ui'
 **
-** Created by: Qt User Interface Compiler version 5.15.13
+** Created by: Qt User Interface Compiler version 5.15.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
